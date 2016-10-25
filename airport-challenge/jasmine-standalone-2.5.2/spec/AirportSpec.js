226bea9945248e36ab165ab8c568d6ca640d5654
your name <akram.rasikh@gmail.com>
2016-10-25 13:39:25 +0100

    new airport javascript

---
+++ airport-challenge/jasmine-standalone-2.5.2/spec/AirportSpec.js
@@ -0,0 +1,21 @@
+describe("Airport", function() {
+  var airport;
+  var airplane;
+
+  beforeEach(function() {
+    airport = new Airport();
+    airplane = new Airplane();
+  });
+});
+
+
+//
+//
+// describe("Player", function() {
+//   var player;
+//   var song;
+//
+//   beforeEach(function() {
+//     player = new Player();
+//     song = new Song();
+//   });
